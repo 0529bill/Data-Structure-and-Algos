287f3a13a8807ba28a30497e42b236ef2c955d4d
bill <0529bilL@gmail.com>
2022-10-31 10:05:02 +0800

    feat: BST

---
+++ Data Structure/Trees/Tree.js
@@ -0,0 +1,78 @@
+/**Time complexity
+Access	O(log(n))
+Search	O(log(n))
+Insert	O(log(n))
+Remove	O(log(n))
+**/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+  find(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+var tree = new BinarySearchTree();
+tree.insert(10);
--- Data Structure/cheatsheet.md
+++ Data Structure/cheatsheet.md
@@ -12,9 +12,13 @@
 - no other character in the string => reverse then compare
 - remove other character and check if it's a palindrome => two pointer, one from the beginning and one from the end
 
-## substring
+### substring
 
 ### Search
 
 - use binary search for sorted array.
 - use two pointers to implement binary search. ex, leetcode 704
+
+## stack and queue
+
+- Valid Parentheses problem(20) => use stack to copy the first part of the array then pop it, if it's not the same as the second part, it's not a valid Parentheses
